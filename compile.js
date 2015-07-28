5453836520bf53ec3a48a7852f11109d4aca1e5c
Paul Podlech <podlech@daz-services.de>
2015-07-28 13:21:42 +0200

    added maintenance files

---
+++ compile.js
@@ -0,0 +1,21 @@
+var cc = require('closure-compiler');
+var fs = require('fs');
+var fileName = 'dist/dcDaD.min.js';
+
+var options =
+  {
+    compilation_level: 'ADVANCED_OPTIMIZATIONS'
+  };
+
+function aftercompile (err, stdout, stderr) {
+  if (err) throw err;
+  var mycompiledcode = stdout;
+  fs.writeFile(fileName, mycompiledcode, function(err){
+    if(err){
+      return console.log(err);
+    }
+    console.log('File compiled and saved as: ' + fileName);
+  });
+}
+
+cc.compile(fs.readFileSync('src/dcDragAndDrop.js'), options, aftercompile);
